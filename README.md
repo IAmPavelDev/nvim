984fe1f5323a82c17e41440384b4eee66bb82c09
IAmPavelDev <179werpavel@protonmail.com>
2024-07-03 21:55:53 +0300

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+Neovim config
+
+Based on Primeagean's nvim config:
+https://github.com/ThePrimeagen/init.lua
+
+I've added some useful plugins and removed some that was annoying.
+
+#skill_issue
+
+useful links:
+
+https://github.com/ThePrimeagen/harpoon
+https://youtu.be/w7i4amO_zaE?si=eafZn0dCH9JAe8yI
---
+++ lua/iampaul/packer.lua
@@ -0,0 +1,131 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd('packer.nvim')
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    --[[ use({ ]]
+    --[[  'rose-pine/neovim', ]]
+    --[[  as = 'rose-pine', ]]
+    --[[  config = function() ]]
+    --[[   vim.cmd('colorscheme rose-pine') ]]
+    --[[  end ]]
+    --[[ }) ]]
+
+    use("rebelot/kanagawa.nvim")
+
+    use {
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup({
+                icons = false,
+            })
+
+            vim.keymap.set("n", "<leader>tt", function()
+                require("trouble").toggle()
+            end)
+
+            vim.keymap.set("n", "[t", function()
+                require("trouble").next({ skip_groups = true, jump = true });
+            end)
+
+            vim.keymap.set("n", "]t", function()
+                require("trouble").previous({ skip_groups = true, jump = true });
+            end)
+        end
+    }
+
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end, }
+    use("nvim-treesitter/playground")
+    use("theprimeagen/refactoring.nvim")
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use("nvim-treesitter/nvim-treesitter-context");
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
+
+    use("folke/zen-mode.nvim")
+    --[[ use("github/copilot.vim") ]]
+    --[[ use("eandrju/cellular-automaton.nvim") ]]
+    use("laytan/cloak.nvim")
+
+    use('neovim/nvim-lspconfig')
+    use('jose-elias-alvarez/null-ls.nvim')
+    use('MunifTanjim/prettier.nvim')
+
+    use {
+        'Exafunction/codeium.vim',
+        config = function()
+            -- Change '<C-g>' here to any keycode you like.
+            vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+            vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+                { expr = true, silent = true })
+            vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+                { expr = true, silent = true })
+            vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+        end
+    }
+
+    use { 'neoclide/coc.nvim', branch = 'release' }
+
+    use("lukas-reineke/indent-blankline.nvim")
+
+    use("numToStr/Comment.nvim")
+
+    use("nvim-lua/plenary.nvim")
+
+    use("windwp/nvim-autopairs")
+
+    use("JoosepAlviste/nvim-ts-context-commentstring")
+
+    use('lewis6991/gitsigns.nvim')
+
+    use "sindrets/diffview.nvim"
+
+    use "nvim-tree/nvim-tree.lua"
+
+    use "nvim-tree/nvim-web-devicons"
+
+    --[[ use('echasnovski/mini.pairs') ]]
+end)
